dda93ba59c44d37bfd1cbd248d3b96df52ea292e
gnee-code <george.needham@manchester.ac.uk>
2022-07-13 19:14:06 +0100

    Begin to define kit class

---
+++ utilities.py
@@ -0,0 +1,42 @@
+################################################################
+#                                                              
+# RAMPLE-KITMAN: A web app designed to generate 'kits' for the 
+#                Squarp Rample Eurorack module.                
+#
+# utilities.py: Contains functions for saving and loading
+#               sample files.
+#
+# Copyright (C) 2022 George N
+################################################################
+
+# Imports
+import os
+import numpy as np
+import librosa
+import pydub as pd
+
+# Define a kit class to store the data
+class kit:
+
+    layout = [[],[],[],[]]
+
+    def __init__(self, letter, number):
+        self.letter = letter.upper()
+        self.number = number
+        self.name = letter + str(number)
+
+
+    def AddMonoSample(self, sample, slot):
+        self.layout[slot].append(sample)
+
+    def AddStereoSample(self, sample, slot):
+        sample_left = sample[0]
+        sample_right = sample[1]
+        self.layout[slot].append(sample[0])
+        self.layout[slot+1].append(sample[1])
+
+    def RemoveSample(self, slot, index):
+        self.layout[slot].pop(index)
+
+    
+     
